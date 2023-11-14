b37ebce6beb1b165720d2ac22179500cf0202a8e
Yassa401 <assalyasser001@gmail.com>
2023-11-14 12:10:54 +0100

    Changements dans classe Soldat
    Ajout d'une variable Position qui stocke la position de soldat, lorsqu'on appelle le constructeur on genere aleatoirement la position du soldat

--- src/wargame/Soldat.java
+++ src/wargame/Soldat.java
@@ -1,5 +1,43 @@
 package wargame;
 
 abstract class Soldat implements ISoldat {
+	 private int pointsDeVie;
+	    private int tour;
+	    private int portee;
+	    private Position position;
 
+	    Soldat(int pointsDeVie, int portee) {
+	        this.pointsDeVie = pointsDeVie;
+	        this.tour = 0;
+	        this.portee = portee;
+	        this.position = new Position(PanneauJeu.dimension);
+	    }
+
+	    public int getPoints() {
+	        return pointsDeVie;
+	    }
+
+	    public int getTour() {
+	        return tour;
+	    }
+
+	    public int getPortee() {
+	        return portee;
+	    }
+	    
+	    public Position getPosition() {
+	    	return position;
+	    }
+
+	    public void joueTour(int tour) {
+	        this.tour = tour;
+	    }
+
+	    public void seDeplace(Position nouvelle_Pos) {
+	        this.position =  nouvelle_Pos;
+	    }
+
+	    public void combat(Soldat soldat) {
+	        // code qui gere le combat
+	    }
 }
